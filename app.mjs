57237ed729afa9e80d8774b8560bfce3b8be72df
demonstratorium <steffen.wolf@gmx.net>
2023-07-19 22:51:23 +0200

    a and test

---
+++ app.mjs
@@ -0,0 +1,2 @@
+import {_} from 'lib.mjs';
+console.log(` ${_.U.version()}  ${_.D.version()}  ${_.X.version()} `);
---
+++ lib.mjs
@@ -0,0 +1,7 @@
+const _ ={
+  X : { version : ()=>`X Module`}
+, U : { version : ()=>`U Module`}
+, D : { version : ()=>`D Module`}
+, I : { version : ()=>`I Module`}
+};
+export {_}
